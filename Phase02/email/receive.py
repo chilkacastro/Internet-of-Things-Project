f011dad14a67a32249d3d95ff4daa4d8b1e957f2
NicholasCiobanu <ciobanunicholas@gmail.com>
2022-10-21 22:44:41 -0400

    worked on email part

--- Phase02/email/receive.py
+++ Phase02/email/receive.py
@@ -3,14 +3,24 @@ import imaplib
 import getpass
 EMAIL = 'nicholasciobanu@outlook.com'
 PASSWORD = getpass.getpass('Password:')
-print("connection successful")
+
 SERVER = 'outlook.office365.com'
+
+def activateFan():
+    print("ACTIVATE THE FAN")
+
+message = ''
+mail_content = ''
+#SETUP PERMANENT EMAIL AND HARD CODED PASSWORD
 while True:
     mail = imaplib.IMAP4_SSL(SERVER)
     mail.login(EMAIL, PASSWORD)
     mail.select('inbox')
-    #status, data = mail.search(None,'(FROM "the email we will use" SUBJECT "FAN CONTROL" UNSEEN)')
-    status, data = mail.search(None,'(SUBJECT "FAN CONTROL" UNSEEN)')
+    
+    status, data = mail.search(None,'(FROM "nicholasciobanu@outlook.com" SUBJECT "FAN CONTROL" UNSEEN)')
+    #status, data = mail.search(None,'(SUBJECT "FAN CONTROL" UNSEEN)')
+
+    #most of this is useless stuff, check the comments 
     mail_ids = []
     for block in data:
         mail_ids += block.split()
@@ -22,14 +32,23 @@ while True:
                 message = email.message_from_bytes(response_part[1])
                 mail_from = message['from']
                 mail_subject = message['subject']
+                
                 if message.is_multipart():
-                    mail_content = ''
-
                     for part in message.get_payload():
                         if part.get_content_type() == 'text/plain':
-                            mail_content += part.get_payload()
+                            #THE CODE SHOULD ALWAYS END UP HERE, CAN'T CONVERT mail_content to string
+                            mail_content += part.as_string()
+                            #mail_content += part.get_payload()
+                            print(f'{mail_content}')
                 else:
                     mail_content = message.get_payload()
-                print(f'From: {mail_from}')
-                print(f'Subject: {mail_subject}')
-                print(f'Content: {mail_content}')
+                    print(f'mail_content is defined')
+                    
+    if mail_content == "YES" or message == "YES":
+        activateFan()
+        print(f'From: {mail_from}')
+        print(f'Subject: {mail_subject}')
+        print(f'Content: {mail_content}')
+                    
+
+
--- Phase02/email/send.py
+++ Phase02/email/send.py
@@ -1,20 +1,29 @@
 import smtplib, ssl, getpass
 
-port = 587  # For starttls
-smtp_server = "smtp-mail.outlook.com"
-sender_email = "nicholasciobanu@outlook.com"
-receiver_email = "nicholasciobanu@outlook.com"
-password = getpass.getpass('Password:')
-print('Subject:')
-subject = "Subject: " + input()
-print('Body:')
-body = input()
-message = subject + '\n\n' + body
 
-context = ssl.create_default_context()
-with smtplib.SMTP(smtp_server, port) as server:
-    server.ehlo()  # Can be omitted
-    server.starttls(context=context)
-    server.ehlo()  # Can be omitted
-    server.login(sender_email, password)
-    server.sendmail(sender_email, receiver_email, message)
+
+#code to send the email
+def sendEmail():
+    port = 587  # For starttls
+    smtp_server = "smtp-mail.outlook.com"
+    sender_email = "nicholasciobanu@outlook.com"
+    receiver_email = "nicholasciobanu@outlook.com"
+    #create a new account and set a hardcoded password
+    password = getpass.getpass('Password:')
+    subject = "Subject: FAN CONTROL" 
+    body = "Your home temperature is greater than 24. Do you wish to turn on the fan. Reply YES if so."
+    message = subject + '\n\n' + body
+    context = ssl.create_default_context()
+    with smtplib.SMTP(smtp_server, port) as server:
+        server.ehlo()  # Can be omitted
+        server.starttls(context=context)
+        server.ehlo()  # Can be omitted
+        server.login(sender_email, password)
+        server.sendmail(sender_email, receiver_email, message)
+
+
+#put in an infinite loop or something, find a way to feed the temperature data to this method
+#if ("THE TEMPERATURE IS ABOVE 24"):
+#   sendEmail()
+
+

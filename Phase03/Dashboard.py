5d24c6e19965a6c5ab9a398462db6bd966220fcb
Chilka Castro <chilcjcastro@yahoo.ca>
2022-11-06 18:19:23 -0500

    Added led lighting to publisher in Arduino

--- Phase03/Dashboard.py
+++ Phase03/Dashboard.py
@@ -17,17 +17,19 @@ from datetime import datetime
 # broker = '192.168.0.158' #ip in Lab class
 broker = '192.168.1.110'
 port = 1883
-topic = "esp/lightintensity"
+topic1 = "esp/lightintensity"
+topic2 = "esp/lightswitch"
 # generate client ID with pub prefix randomly
 client_id = f'python-mqtt-{random.randint(0, 100)}'
 # username = 'emqx'
 # password = 'public'
 esp_message = 0
+esp_lightswitch_message = ""
 GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
 LedPin = 35 # Led Pin/Enable Pin
 GPIO.setup(LedPin,GPIO.OUT)
-email_counter = 0    # just checks if email has been sent at some stage
+#email_counter = 0    # just checks if email has been sent at some stage
 # -----------------------------------------------
 
 #------------PHASE02 VARIABLE CODES--------------
@@ -228,50 +230,52 @@ app.layout = html.Div([nav_menu,
 #     return (temperature * 1.8) + 32
 
 # PHASE 03 CODE FOR SUBSCRIBE 
-def sendEmail():
-        port = 587  # For starttls
-        smtp_server = "smtp-mail.outlook.com"
-        sender_email = "iotdashboard2022@outlook.com"
-        receiver_email = "iotdashboard2022@outlook.com"
-        password = 'iotpassword123'
-        subject = "Subject: LIGHT NOTIFICATION" 
-        current_time = datetime.now()
-        time = current_time.strftime("%H:%M")
-        body = "The Light is ON at " + time
-        message = subject + '\n\n' + body
-        context = ssl.create_default_context()
-        with smtplib.SMTP(smtp_server, port) as server:
-            server.ehlo()  # Can be omitted
-            server.starttls(context=context)
-            server.ehlo()  # Can be omitted
-            server.login(sender_email, password)
-            server.sendmail(sender_email, receiver_email, message)
-            
+# def sendEmail():
+#         port = 587  # For starttls
+#         smtp_server = "smtp-mail.outlook.com"
+#         sender_email = "iotdashboard2022@outlook.com"
+#         receiver_email = "iotdashboard2022@outlook.com"
+#         password = 'iotpassword123'
+#         subject = "Subject: LIGHT NOTIFICATION" 
+#         current_time = datetime.now()
+#         time = current_time.strftime("%H:%M")
+#         body = "The Light is ON at " + time
+#         message = subject + '\n\n' + body
+#         context = ssl.create_default_context()
+#         with smtplib.SMTP(smtp_server, port) as server:
+#             server.ehlo()  # Can be omitted
+#             server.starttls(context=context)
+#             server.ehlo()  # Can be omitted
+#             server.login(sender_email, password)
+#             server.sendmail(sender_email, receiver_email, message)
+#             
  
-def turn_led_on(value):         # turn led on depending on value send email and increase the email counter to know there is an email sent
-     if value < 400:
-        GPIO.output(LedPin, True)
-        sendEmail()
-        email_counter += 1
-     else:
-        GPIO.output(LedPin, False)  
+# def turn_led_on(value):         # turn led on depending on value send email and increase the email counter to know there is an email sent
+#      if value < 400:
+#         GPIO.output(LedPin, True)
+#         sendEmail()
+#         email_counter += 1
+#      else:
+#         GPIO.output(LedPin, False)  
 
 @app.callback(Output('light-intensity', 'value'), Input('light-intensity-update', 'n_intervals'))  
 def update_output(value):
     run()
     # print("Here: ", esp_message) UNCOMMENT TO SEE THE VALUE PASSED FROM THE PUBLISHER 
     value = esp_message
-    turn_led_on(value)              # turn led on and send email.
+#     turn_led_on(value)              # turn led on and send email.
     return value
 
-@app.callback(Output('email_h1', 'children'), Input('interval_component', 'n_intervals'))       # update email sent message
-def update_email_status(n):
-     if email_counter > 0:
-         return "Email has been sent."
-     
-     else:
-         return "No email has been sent."
+# @app.callback(Output('email_h1', 'children'), Input('interval_component', 'n_intervals'))       # update email sent message
+# def update_email_status(n):
+#      if email_counter > 0:
+#          return "Email has been sent."
+#      
+#      else:
+#          return "No email has been sent."
 
+# def on_connect(client, userdata, flags, rc):
+#    print("Connected With Result Code " + (rc))
 
 def connect_mqtt() -> mqtt_client:
     def on_connect(client, userdata, flags, rc):
@@ -280,24 +284,47 @@ def connect_mqtt() -> mqtt_client:
             time.sleep(5)
         else:
             print("Failed to connect, return code %d\n", rc)
-
     client = mqtt_client.Client(client_id)
     client.on_connect = on_connect
     client.connect(broker, port)
     return client
 
-def subscribe(client: mqtt_client):
-    def on_message(client, userdata, msg):
-        global esp_message
-        esp_message = int(float(msg.payload.decode()))
-        
-    client.subscribe(topic)
-    client.on_message = on_message
-    
+def on_message_from_lightintensity(client, userdata, message):
+   global esp_message
+   esp_message = int(float(message.payload.decode()))
+   print("Message Received from LightSwtch: ")
+   print(esp_message)
+
+def on_message_from_lightswitch(client, userdata, message):
+   global esp_lightswitch_message
+   esp_lightswitch_message = message.payload.decode()
+   print("Message Recieved from lightswitch: ")
+   print(esp_lightswitch_message)
+
+def on_message(client, userdata, message):
+   print("Message Received from Others: "+message.payload.decode())
+
 def run():
     client = connect_mqtt()
-    subscribe(client)
+    client.subscribe(topic1, qos=1)
+    client.subscribe(topic2, qos=1)
+    client.message_callback_add(topic1, on_message_from_lightintensity)
+    client.message_callback_add(topic2, on_message_from_lightswitch)
     client.loop_start()
+# 
+
+# def subscribe(client: mqtt_client):
+#     def on_message(client, userdata, msg):
+#         global esp_message
+#         print(esp_message)
+#         esp_message = int(float(msg.payload.decode()))
+#         
+#     client.on_message = on_message
+#     
+# def run():
+#     client = connect_mqtt()
+#     subscribe(client)
+#     client.loop_start()
 
 if __name__ == '__main__':
     app.run_server(debug=False,dev_tools_ui=False,dev_tools_props_check=False)
--- Phase03/Light_Intensity_Publisher/Light_Intensity_Publisher.ino
+++ Phase03/Light_Intensity_Publisher/Light_Intensity_Publisher.ino
@@ -5,6 +5,8 @@
 //const char* ssid = "TP-Link_2AD8";
 //const char* password = "14730078";
 //const char* mqtt_server = "192.168.0.158"
+
+// Chilka: I'm testing this at home so just change this three lines if you want to test it too
 const char* ssid = "EBOX-9994";
 const char* password = "97479ec13d";
 const char* mqtt_server = "192.168.1.110";
@@ -13,8 +15,10 @@ PubSubClient client(vanieriot);
 
 // light setup
 const int pResistor = A0;
+const int ledPin = 16; // equivalent D0
 // Variables
 int value;
+char* ledStatus = "OFF";
 
 void setup_wifi() {
   delay(10);
@@ -66,7 +70,8 @@ void setup() {
   client.setServer(mqtt_server, 1883);
   client.setCallback(callback);
   Serial.println();
-  
+  pinMode(ledPin, OUTPUT);
+  pinMode(pResistor, INPUT);
 }
 
 void loop() {
@@ -78,13 +83,27 @@ void loop() {
 
 // get value of the light intensity
   value = analogRead(pResistor);
+  delay(10000);
+  digitalWrite(ledPin, LOW);
   Serial.println("Light intensity is: ");
   Serial.println(value);
 
-  //publish value
-  char pResistorValue[8];
-  dtostrf(value, 6,2, pResistorValue);
-  
-  client.publish("esp/lightintensity", pResistorValue);
+  if (value >= 400) {
+    digitalWrite(ledPin, HIGH);
+    ledStatus = "ON";
+    Serial.println("Led Status Activated: ");
+    Serial.println(ledStatus);
+    delay(3000);
+  }
+//  else {
+//    digitalWrite(ledPin, LOW);
+//    ledStatus = "OFF";
+//  }
+   //publish value
+   char pResistorValue[8];
+   dtostrf(value, 6,2, pResistorValue); 
+    
+   client.publish("esp/lightintensity", pResistorValue);
+   client.publish("esp/lightswitch", ledStatus);
   
 }
